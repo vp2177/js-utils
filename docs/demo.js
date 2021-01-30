fcd232ad6ac1be97b2f5898e02b851f177c921c7
p <>
2021-01-30 05:58:23 +0000

    +lit-element demo, but it doesn't work :(

--- docs/demo.js
+++ docs/demo.js
@@ -1,7 +1,18 @@
-// import Lit from "https://cdn.jsdelivr.net/npm/lit-element@2.4"
-// ^This actually fails because it imports "lit-html/lib/shady-render.js"..
+import * as L from "https://cdn.jsdelivr.net/npm/lit-element@2.4/+esm"
+// import * as P from "https://cdn.jsdelivr.net/npm/preact@10.5" // TODO: /compat?
 import {appendRoot} from "https://cdn.jsdelivr.net/gh/vp2177/js-utils/append-root.min.js"
 
 
+class A extends L.LitElement {
+ render() {
+     return L.html`<h2>Lit</h2>` // TODO: [object Object]?
+ }
+}
+customElements.define("my-a", A);
 
-appendRoot()
+
+
+
+
+
+
