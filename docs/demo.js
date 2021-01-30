1c0c9687e34e50b1c00c28fd4c42ca168ba7080a
p <>
2021-01-30 04:17:25 +0000

    fix Lighthouse issues in demo html

---
+++ docs/demo.js
@@ -0,0 +1,7 @@
+// import Lit from "https://cdn.jsdelivr.net/npm/lit-element@2.4"
+// ^This actually fails because it imports "lit-html/lib/shady-render.js"..
+import {appendRoot} from "https://cdn.jsdelivr.net/gh/vp2177/js-utils/append-root.min.js"
+
+
+
+appendRoot()
