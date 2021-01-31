58bc52e74defa14060c7a63482e866814651b99e
p <>
2021-01-31 02:18:29 +0000

    try raw lit-html

--- src/jsdelivr-lit-element-issue.js
+++ src/jsdelivr-lit-element-issue.js
@@ -1,13 +1,23 @@
-import * as L from "https://cdn.jsdelivr.net/npm/lit-element@2.4/+esm";
+// import * as L from "https://cdn.jsdelivr.net/npm/lit-element@2.4/+esm";
 // import * as L from "https://cdn.skypack.dev/lit-element@2.4";
-// import * as P from "https://cdn.jsdelivr.net/npm/preact@10.5" // TODO: /compat?
-// import { appendRoot } from "https://cdn.jsdelivr.net/gh/vp2177/js-utils/append-root.min.js";
+import * as H from "https://cdn.jsdelivr.net/npm/lit-html@1.3/+esm"
+import {appendRoot} from "./append-root"
 
+
+H.render(H.html`<h2>Lit HTML</h2>`, appendRoot())
+
+
+
+/*
 class A extends L.LitElement {
   render() {
-    return L.html`<h2>Lit</h2>`; // TODO: Renders [object Object]?
+    return L.html`<h2>Lit</h2>`; // TODO: Renders [object Object] when loaded from jsDelivr?
   }
 }
 customElements.define("my-a", A);
+ 
+appendRoot("my-a")
+*/
 
-document.body.appendChild(document.createElement("my-a"));
+
+
