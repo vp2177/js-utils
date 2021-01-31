1d682695d87aec4d374fde64280c6ed0bb85da72
p <>
2021-01-31 02:53:46 +0000

    log the object returned from html()

--- src/jsdelivr-lit-element-issue.js
+++ src/jsdelivr-lit-element-issue.js
@@ -1,23 +1,22 @@
-// import * as L from "https://cdn.jsdelivr.net/npm/lit-element@2.4/+esm";
+import * as L from "https://cdn.jsdelivr.net/npm/lit-element@2.4/+esm";
 // import * as L from "https://cdn.skypack.dev/lit-element@2.4";
-import * as H from "https://cdn.jsdelivr.net/npm/lit-html@1.3/+esm"
-import {appendRoot} from "./append-root"
-
-
-H.render(H.html`<h2>Lit HTML</h2>`, appendRoot())
+import {appendRoot} from "./append-root.js"
 
 
 
-/*
+
 class A extends L.LitElement {
   render() {
-    return L.html`<h2>Lit</h2>`; // TODO: Renders [object Object] when loaded from jsDelivr?
+    // TODO: Renders [object Object] when loaded from jsDelivr?
+    const res = L.html`<mark>Lit</mark>`; 
+    console.debug(res)
+    return res;
   }
 }
 customElements.define("my-a", A);
  
 appendRoot("my-a")
-*/
+
 
 
 
