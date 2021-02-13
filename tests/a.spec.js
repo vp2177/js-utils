684ac00cb6b6cb19382ef5b7c47d7bb37907af01
p <>
2021-02-13 00:11:08 +0000

    ~

--- tests/a.spec.js
+++ tests/a.spec.js
@@ -1,7 +1,7 @@
-import {describe, it, expect, } from "@jest/globals"
+import { describe, it, expect } from "@jest/globals";
 
 describe("Jest", () => {
-    it("works", () => {
-        return Promise.resolve(true)
-    })
-})
+  it("works", () => {
+    return Promise.resolve(true);
+  });
+});
