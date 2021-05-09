fde6edfe5b836b36aa666dd5a4e4e629007e6bbb
p <vp2177@users.noreply.github.com>
2021-05-09 20:16:40 +0000

    prettier.

--- utils/mock.js
+++ utils/mock.js
@@ -1,7 +1,8 @@
-
-/** 
+/**
  * @param {number} [length]
-*/
+ */
 export function randomString(length) {
-    return Math.random().toString(36).slice(2, length && (2 + length))
+  return Math.random()
+    .toString(36)
+    .slice(2, length && 2 + length);
 }
